ff31e9292bdeb36bc17f6a36a9a63e0d1759609c
Basel Hussain <baselhussain4@gmail.com>
2025-12-21 00:48:30 +0500

    work-remaining

--- components/home/HomePage.jsx
+++ components/home/HomePage.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import HeroSection from "./HeroSection";
 import AboutSection from "./AboutSection";
 import ExcellenceSection from "./ExcellenceSection";
-import ServicesSection from "./ServicesSection";
-import ProjectSection from "./ProjectSection";
 import PerksSection from "./PerksSection";
 import TestimonialsSection from "./TestimonialsSection";
 import CTASection from "./CTASection";
+import ServicesSection from "./ServicesSection";
 
 const HomePage = () => {
   return (
